2c0e93dd97f5659f91eb23b3aacfe1f7292bf649
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2015-06-08 20:13:14 +0100

    Remove again

--- www/app.js
+++ www/app.js
@@ -159,31 +159,6 @@ var app = (function()
 		$(document).ready(function() {
 			addEventHandlers();
 		});
-
-        // Reference the auto-generated proxy for the hub.  
-        var chat = $.connection.chatHub;
-        // Create a function that the hub can call back to display messages.
-        chat.client.addNewMessageToPage = function (name, message) {
-            // Add the message to the page. 
-            $('#discussion').prepend('<li><strong>' + htmlEncode(name) 
-                + '</strong>: ' + htmlEncode(message) + '</li>');
-        };
-        // Set initial focus to message input box.  
-        $('#message').focus();
-        // Start the connection.
-        $.connection.hub.start().done(function () {
-            $('#sendmessage').click(function () {
-                // Call the Send method on the hub. 
-                chat.server.send(userId, $('#message').val());
-                // Clear text box and reset focus for next comment. 
-                $('#message').val('').focus();
-            });
-        });
-        // This optional function html-encodes messages for display in the page.
-        function htmlEncode(value) {
-            var encodedValue = $('<div />').text(value).html();
-            return encodedValue;
-        }
 	};
 
 	function addEventHandlers() 
